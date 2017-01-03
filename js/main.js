7d3447e7b238a7ea63fc8fdf75ba8b4bbd16848a
Russell Comer <rcomer@crimsonquest.com>
2017-01-02 22:46:06 -0500

    Added the files already made.
    Added the original html css and javascript files

---
+++ js/main.js
@@ -0,0 +1,24 @@
+//All variables are initally declared here
+var squadOnePostOneFirst, squadOnePostOneSecond, squadOnePostOneTotal, squadOnePostTwoFirst, squadOnePostTwoSecond, squadOnePostTwoTotal, squadOnePostThreeFirst, squadOnePostThreeSecond, squadOnePostThreeTotal, squadOnePostFourFirst, squadOnePostFourSecond, squadOnePostFourTotal, squadOnePostFiveFirst, squadOnePostFiveSecond, squadOnePostFiveTotal, squadTwoPostOneFirst, squadTwoPostOneSecond, squadTwoPostOneTotal, squadTwoPostTwoFirst, squadTwoPostTwoSecond, squadTwoPostTwoTotal, squadTwoPostThreeFirst, squadTwoPostThreeSecond, squadTwoPostThreeTotal, squadTwoPostFourFirst, squadTwoPostFourSecond, squadTwoPostFourTotal, squadTwoPostFiveFirst, squadTwoPostFiveSecond, squadTwoPostFiveTotal, squadThreePostOneFirst, squadThreePostOneSecond, squadThreePostOneTotal, squadThreePostTwoFirst, squadThreePostTwoSecond, squadThreePostTwoTotal, squadThreePostThreeFirst, squadThreePostThreeSecond, squadThreePostThreeTotal, squadThreePostFourFirst, squadThreePostFourSecond, squadThreePostFourTotal, squadThreePostFiveFirst, squadThreePostFiveSecond, squadThreePostFiveTotal;
+//Squad One Shooter One variables set
+squadOnePostOneFirst = getElementById('squadOnePostOneFirst');
+squadOnePostOneSecond = getElementById('squadOnePostOneSecond');
+squadOnePostOneTotal = squadOnePostOneFirst + squadOnePostOneSecond;
+//Squad One Shooter Two variables set
+squadOnePostTwoFirst = getElementById('squadOnePostTwoFirst');
+squadOnePostTwoSecond = getElementById('squadOnePostTwoSecond');
+
+
+var calculate = function(first25, second25){
+    first25 + second25 = total;
+    return total;
+}
+
+var shooter = {};
+
+var shooter1 = new shooter();
+shooter1.firstScore = squadOnePostOneFirst;
+shooter1.secondScore = squadOnePostOneSecond;
+shooter1.totalScore = calculate(squadOnePostOneFirst,squadOnePostOneSecond);
+
+document.getElementById('squadOnePostOneTotal').innerHTML = squadOnePostOneTotal;
