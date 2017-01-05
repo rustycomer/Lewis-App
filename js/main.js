8b299373165b229fba2f81764179f76b4f810d64
Russell Comer <rcomer@crimsonquest.com>
2017-01-04 20:51:06 -0500

    Added jQuery and fixed at the mainjs functionality
    My attempt to fix the mainjs code to create a constructor function and have the js manipulate the html element total by taking in parameters of the first and second score.

--- js/main.js
+++ js/main.js
@@ -1,24 +1,29 @@
-//All variables are initally declared here
+/*//All variables are initally declared here
 var squadOnePostOneFirst, squadOnePostOneSecond, squadOnePostOneTotal, squadOnePostTwoFirst, squadOnePostTwoSecond, squadOnePostTwoTotal, squadOnePostThreeFirst, squadOnePostThreeSecond, squadOnePostThreeTotal, squadOnePostFourFirst, squadOnePostFourSecond, squadOnePostFourTotal, squadOnePostFiveFirst, squadOnePostFiveSecond, squadOnePostFiveTotal, squadTwoPostOneFirst, squadTwoPostOneSecond, squadTwoPostOneTotal, squadTwoPostTwoFirst, squadTwoPostTwoSecond, squadTwoPostTwoTotal, squadTwoPostThreeFirst, squadTwoPostThreeSecond, squadTwoPostThreeTotal, squadTwoPostFourFirst, squadTwoPostFourSecond, squadTwoPostFourTotal, squadTwoPostFiveFirst, squadTwoPostFiveSecond, squadTwoPostFiveTotal, squadThreePostOneFirst, squadThreePostOneSecond, squadThreePostOneTotal, squadThreePostTwoFirst, squadThreePostTwoSecond, squadThreePostTwoTotal, squadThreePostThreeFirst, squadThreePostThreeSecond, squadThreePostThreeTotal, squadThreePostFourFirst, squadThreePostFourSecond, squadThreePostFourTotal, squadThreePostFiveFirst, squadThreePostFiveSecond, squadThreePostFiveTotal;
+*/
 //Squad One Shooter One variables set
-squadOnePostOneFirst = getElementById('squadOnePostOneFirst');
-squadOnePostOneSecond = getElementById('squadOnePostOneSecond');
-squadOnePostOneTotal = squadOnePostOneFirst + squadOnePostOneSecond;
-//Squad One Shooter Two variables set
-squadOnePostTwoFirst = getElementById('squadOnePostTwoFirst');
-squadOnePostTwoSecond = getElementById('squadOnePostTwoSecond');
+var firstNameSquadOnePostOne = $('#firstNameSquadOnePostOne').val();
+var lastNameSquadOnePostOne = $('#lastNameSquadOnePostOne').val();
+var squadOnePostOneFirst = $('#squadOnePostOneFirst').val();
+var squadOnePostOneSecond = $('#squadOnePostOneSecond').val();
 
 
-var calculate = function(first25, second25){
-    first25 + second25 = total;
-    return total;
-}
 
-var shooter = {};
 
-var shooter1 = new shooter();
-shooter1.firstScore = squadOnePostOneFirst;
-shooter1.secondScore = squadOnePostOneSecond;
-shooter1.totalScore = calculate(squadOnePostOneFirst,squadOnePostOneSecond);
-
-document.getElementById('squadOnePostOneTotal').innerHTML = squadOnePostOneTotal;
+// Constructor function for building the shooter object
+function shooter(firstName, lastName, f25, s25) {
+    this.first25 = f25;
+    this.second25 = s25;
+    this.total = function(f25, s25){
+        var total50 = f25 + s25;
+        return total50;
+    };
+};
+//Setting all of Shooter1 properties
+var shooter1 = new shooter(firstNameSquadOnePostOne, lastNameSquadOnePostOne, squadOnePostOneFirst, squadOnePostOneSecond);
+var squadOnePostOneTotal = shooter1.total;
+$("#squadOnePostOneSecond").change(function(){
+    $("#squadOnePostOneTotal").html(function(){
+        return squadOnePostOneTotal;
+    });
+});  
